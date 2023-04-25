527b18a61af5173c766d1ce5bc17170e0067ca21
Mickey Shaughnessy <mickeyshaughnessy@gmail.com>
2023-04-25 13:03:09 -0600

    add simulator

---
+++ simulate.py
@@ -0,0 +1,13 @@
+# This is a backyard physics simulator
+
+import time
+ 
+def step_simulation():
+    pass
+
+if __name__ == "__main__":
+    while True:
+        step_simulation()
+        print("stepped simulation")
+        time.sleep(1)
+        
